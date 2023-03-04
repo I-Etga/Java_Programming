e8f0586477b08d0ec46433806b94dc1a7f10821f
IEAkay <akayetga@gmail.com>
2023-03-04 04:37:34 +0100

    daily update

--- src/daily_videos/day06_ifStatements/exercises/GiftCard.java
+++ src/daily_videos/day06_ifStatements/exercises/GiftCard.java
@@ -18,41 +18,46 @@ public class GiftCard {
         int panths = 50;
         int pillow = 40;
         int socks = 5;
-        int usaCable = 10;
+        int usbCable = 10;
 
         String result = "Thank you for your purchase!\nYour current balance is: $";
 
+        if (item.equals("laptop") || item.equals("smartphone")) {
+            result = "Sorry, not enough funds on your gift card";
+        } else if (item.equals("blanket") || item.equals("charger")
+                || item.equals("hat") || item.equals("headphones")
+                || item.equals("panths") || item.equals("pillow")
+                || item.equals("socks") || item.equals("usb cable")) {
 
-        switch (item) {
-            case "blanket":
-                result += (balance - blanket);
-                break;
-            case "charger":
-                result += (balance - charger);
-                break;
-            case "hat":
-                result += (balance - hat);
-                break;
-            case "headphones":
-                result += (balance - headphones);
-                break;
-            case "panth":
-                result += (balance - panths);
-                break;
-            case "pillow":
-                result += (balance - pillow);
-                break;
-            case "socks":
-                result += (balance - socks);
-                break;
-            default:
-                result += (balance - usaCable);
-                if (item == "laptop" || item == "smartphone") {
-                    result = "Sorry, not enough funds on your gift card";
-                } else {
-                    result = "Sorry, that is an invalid item";
-                }
+            switch (item) {
+                case "blanket":
+                    result += ((balance - blanket) + "$");
+                    break;
+                case "charger":
+                    result += ((balance - charger) + "$");
+                    break;
+                case "hat":
+                    result += ((balance - hat) + "$");
+                    break;
+                case "headphones":
+                    result += ((balance - headphones) + "$");
+                    break;
+                case "panths":
+                    result += ((balance - panths) + "$");
+                    break;
+                case "pillow":
+                    result += ((balance - pillow) + "$");
+                    break;
+                case "socks":
+                    result += ((balance - socks) + "$");
+                    break;
+                default:
+                    result += ((balance - usbCable) + "$");
+            }
+        } else {
+            result = "Sorry, that is an invalid item";
         }
         System.out.print(result);
     }
+
 }
