0036f96f14fb7fffa570321fe03f4c885dcd9e40
IEAkay <akayetga@gmail.com>
2023-03-13 00:33:30 +0100

    daily update

--- src/other/wednesday_office_hours/week3/week03.java
+++
@@ -1,7 +0,0 @@
-package other.wednesday_office_hours.week3;
-
-public class week03 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/sunday_live_sessions/week03/week03.java
+++
@@ -1,4 +0,0 @@
-package sunday_live_sessions.week03;
-
-public class week03 {
-}
