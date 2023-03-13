8e7bd2300ada5fee678ec28dfdef57e6043a80f2
IEAkay <akayetga@gmail.com>
2023-03-13 22:43:30 +0100

    daily update

--- src/daily_videos/day15_LoopsContinue/ContinueStatement.java
+++ src/daily_videos/day15_LoopsContinue/ContinueStatement.java
@@ -1,8 +1,9 @@
 package daily_videos.day15_LoopsContinue;
+
 public class ContinueStatement {
     public static void main(String[] args) {
-        for (int i = 1; i < 6 ; i++) {
-            if(i == 2 ){
+        for (int i = 1; i < 6; i++) {
+            if (i == 2) {
                 /* Because of continue, it prints 1,3,4,5.
                 So, only 2 is not printed. Before and After continue got executed.
 
@@ -13,5 +14,52 @@ public class ContinueStatement {
             }
             System.out.println(i);
         }
+
+        System.out.println("**********");
+
+        for (int i = 10; i < 21; i++) {
+
+            if (i % 2 == 0) {
+                continue; // skip the current iteration
+                // With this continue statement, it will print only odd numbers.
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("**********");
+
+        for (char i = 'A'; i <= 'G'; i++) {
+            /*
+            if(i == 'A' & i == 'B'){ // i can NOT be two characters
+                        at the SAME TIME. So always false and never jumps the next i !!
+                continue;
+            }
+             */
+            if (i == 'B') {
+                continue;
+            }
+            if (i == 'E') {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("**********");
+
+        for (int i = 0; i < 10; i++)
+            System.out.println("Hello World");
+        System.out.println("hi");
+        System.out.println("Hello Cydeo");
+
+            /*
+                    LOOP WİTHOUT CURLY BRACES ( NOT RECOMMENDED !!)
+            This is for loop without curly braces.
+            It executes ONLY NEXT statement.
+            So line 49 will be printed 10 TIMES !!
+            and line 50-51 will be printed/executed only ONCE !!(compiler warns! anyway)
+
+             */
+
+
     }
 }
