6b1f5025da2a4e3f2438307aca1c321dce963b1d
IEAkay <akayetga@gmail.com>
2023-03-14 17:38:14 +0100

    daily update

--- src/daily_videos/day16_loopsContinue/CydeoLogIn.java
+++ src/daily_videos/day16_loopsContinue/CydeoLogIn.java
@@ -17,17 +17,24 @@ public class CydeoLogIn {
 
         if (userName.equals(userNameInput) && password.equals(passwordInput)) {
             System.out.println("Logged in ");
-        }
-
-        if (!userName.equals(userNameInput) || !password.equals(passwordInput)) {
+        } else {
             int i = 2;
-            while (i > 0) {
+            for (i = 1; i >= 0; i--) {
                 System.out.println("username or password is not correct!!\nRemaining attempts:" + i + "\nPlease re-enter your username: ");
                 userNameInput = scanner.next();
                 System.out.println("Please re-enter your password: ");
                 passwordInput = scanner.next();
-                i--;
+                /*
+                        !!! ATTENTION !!!
+                Difference between FOR and WHILE
 
+                # iteration = UNKNOWN[] --> WHILE
+                # iteration = known how many times we need to use -->FOR
+
+                int i =2 ;
+                while(i<0){
+                 }
+                 */
                 if (i == 0) {
                     System.out.println("You're banned to log in!! ");
                 }
