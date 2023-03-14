93b7a376dcdedf4c91b2ef834950bfcc6b37ba17
IEAkay <akayetga@gmail.com>
2023-03-14 19:06:19 +0100

    daily update

--- src/daily_videos/day16_loopsContinue/CydeoLogIn.java
+++ src/daily_videos/day16_loopsContinue/CydeoLogIn.java
@@ -31,12 +31,25 @@ public class CydeoLogIn {
                 # iteration = UNKNOWN[] --> WHILE
                 # iteration = known how many times we need to use -->FOR
 
+                # With while()
                 int i =2 ;
                 while(i<0){
                  }
                  */
+                /*
+                We have to use matching condition two times.(inside and outside loops)
+                Because if we at the first attempt enter wrong, loops starts.
+
+                Even if we enter the matching data, it stays in loop
+                                and doesn't change the first(in case they are matching) statement.
+
+                So we write the first if condition once more inside loop.
+                 */
+                if (userName.equals(userNameInput) && password.equals(passwordInput)) {
+                    System.out.println("Logged in ");
+                    break;}
                 if (i == 0) {
-                    System.out.println("You're banned to log in!! ");
+                    System.out.println("You account now is locked.Please contact with the support team");
                 }
             }
         }
