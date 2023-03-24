29647f75e2ef0b8c54ca5543679f22d41339e8cf
i.etga <akayetga@gmail.com>
2023-03-24 17:52:46 +0100

    daily update

--- src/daily_videos/day15_loopsContinue/practice/HotelReservation.java
+++ src/daily_videos/day15_loopsContinue/practice/HotelReservation.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class HotelReservation {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Would you like to book a room?");
         String answer = scanner.next().toLowerCase();
 
@@ -13,32 +13,35 @@ public class HotelReservation {
             System.out.println("Invalid entry.Please enter yes/no:");
             answer = scanner.next();
         }
-
         if (answer.equals("no")) {
             System.out.println("Have a nice day. Goodbye!");
             return;
-        } else {
+        }else{
             System.out.println("Which room would you like to book?\nKing bed ==>120$ \nQueen Bed ==> 100$\nSingle Bed==>80$");
-            answer = scanner.next();
+            scanner.nextLine();
+            answer = scanner.nextLine();
+
             while (!(answer.equalsIgnoreCase("King Bed")
-                    || answer.equalsIgnoreCase("King")
                     || answer.equalsIgnoreCase("Queen Bed")
-                    || answer.equalsIgnoreCase("Queen")
-                    || answer.equalsIgnoreCase("Single Bed")
-                    || answer.equalsIgnoreCase("Single"))) {
-                System.out.println("Invalid entry.Please enter again ! (Single, King, Single Bed)");
-                answer = scanner.next();
+                    || answer.equalsIgnoreCase("Single Bed"))) {
+
+                System.out.println("Invalid entry.Please enter again ! (Single Bed, King Bed or Queen Bed)");
+                answer = scanner.nextLine();
             }
         }
-        String message = "";
-        if (answer.equalsIgnoreCase("King") || answer.equalsIgnoreCase("King Bed")) {
-            message = "You've reserved a Kind Bed room. And the total price of this room: 120$";
-        } else if (answer.equalsIgnoreCase("Queen") || answer.equalsIgnoreCase("Queen Bed")) {
-            message = "You've reserved a Queen Bed room. And the total price of this room: 100$";
-        } else {
-            message = "You've reserved a Single Bed room. And the total price of this room: 80$";
-        }
+        String message = "You've reserved a ";
+        int price = 0;
 
+        if ( answer.equalsIgnoreCase("King Bed")) {
+            price = 120;
+            message += answer + " room. And the total price of this room; "+ price + "$";
+        } else if ( answer.equalsIgnoreCase("Queen Bed")) {
+            price = 100;
+            message += answer+ " room. And the total price of this room; "+ price + "$";
+        } else {
+            price = 80;
+            message += answer + " room. And the total price of this room; "+ price + "$";
+        }
         System.out.println(message);
     }
 }
--- src/daily_videos/day15_loopsContinue/practice/LogIn.java
+++ src/daily_videos/day15_loopsContinue/practice/LogIn.java
@@ -12,22 +12,24 @@ public class LogIn {
         System.out.println("Please enter your username");
         String usernameInp = scanner.next();
         System.out.println("Please enter your password");
-        String passwordInp= scanner.next();
+        String passwordInp = scanner.next();
 
-        int count = 3;
-        while (!((username.equals(usernameInp)) && password.equals(passwordInp))){
-            count--;
-            System.out.println("Incorrect username or password. Try again!\n" + count +" attempts left!");
-            if(count == 1){
-                System.out.println("Last attempt");
-            }if(count == 0){
+        int count = 2;
+        //!false                !true
+        //     ( true      &&            true)
+        while (!((username.equals(usernameInp)) && password.equals(passwordInp))) {
+            System.out.println("Incorrect username or password.\n" + count + " attempts left!");
+
+            if (count == 0) {
                 System.out.println("Your account is locked !");
+                return; // If you use break, cursor jump up the next line after while loop. return or System.exit(1) exit program
             }
 
             System.out.println("Please enter your username");
             usernameInp = scanner.next();
             System.out.println("Please enter your password");
-            passwordInp= scanner.next();
+            passwordInp = scanner.next();
+            count--;
         }
         System.out.println("Logged in ...");
     }
