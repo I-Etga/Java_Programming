7d7616090f848a3923055e2a4ad9dc86d54a69fe
i.etga <akayetga@gmail.com>
2023-03-26 03:16:22 +0200

    daily update

--- src/sundays_live_sessions/week7/Countries.java
+++ src/sundays_live_sessions/week7/Countries.java
@@ -1,39 +1,70 @@
 package sundays_live_sessions.week7;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Countries {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         String[] countries = {
                 "Brazil", "China", "Cuba", "Sweden", "France", "Vietnam",
-                "Albania", "Portugal", "Philippines", "Armenia",  "Colombia",
+                "Albania", "Portugal", "Philippines", "Armenia", "Colombia",
                 "Honduras", "Indonesia", "United States"
         };
 
+        String[] europa = {"Germany", "Belgium", "France", "Netherlands", "Poland", "Spain", "Italy", "Norway", "Sweden"};
 
+        // #1 first and last char of countries
+        for (String country : countries) {
+            System.out.print("" + country.charAt(0) + country.charAt(country.length() - 1) + " ");
+        }
+        System.out.println("-------------");
+
+        // #2 largest and shortest named countries
+        int longest = 0;
+        int shortest = 100; // try to write the number of the largest country all over the world !!
+
+        String longestCountry = "";
+        String shortestCountry = "";
+
+        for (String country : countries) {
+
+            if (country.length() > longest) {
+                longest = country.length();
+                longestCountry = country;
+            }
+            if (country.length() < shortest) {
+                shortest = country.length();
+                shortestCountry = country;
+            }
+        }
+        System.out.println("shortestCountry = " + shortestCountry);
+        System.out.println("longestCountry = " + longestCountry);
+
+        System.out.println("-----------");
+        // #3 starts with 'C'
+        System.out.println("Counties start with \"C\" in this array: ");
+        for (String country : countries) {
+            if (country.startsWith("c") || country.startsWith("C")) {
+                System.out.print(country + " ");
+            }
+        }
+        System.out.println();
+        // #4 ends with 's'
+        System.out.println("--------");
+        System.out.println("Counties ends with \"s\" in this array: ");
+        for (String country : countries) {
+            if (country.endsWith("s")) {
+                System.out.print(country + " ");
+            }
+        }
+        // #5 put counties in order
+        System.out.println();
+        System.out.println("-----------");
+
+        Arrays.sort(countries); // this is a void method. So we can't assign it to itself of to another array by using sort method!
+        System.out.println(Arrays.toString(countries));
 
     }
 }
-
-/*
-Countries [array, String, loops]
-
-    -Create a program that defines an array of countries.
-    -Use the countries defined below or create your own array that has different country names.
-    -Use the countries to display different information defined in the mini tasks
-
-        String[] countries = {
-                "Brazil", "China", "Cuba", "Sweden", "France", "Vietnam",
-                "Albania", "Portugal", "Philippines", "Armenia",  "Colombia",
-                "Honduras", "Indonesia", "United States"
-                };
-
-	- Find and print all the first and last characters
-
-	- Find the largest and smaller countries based on the length of their names
-
-    - Show all the countries that start with a 'C'
-
-    - Show all the countries that end with an 's'
-
-    - Show the countries names in alphabetical order
- */
--- src/sundays_live_sessions/week7/ReverseMiddle.java
+++ src/sundays_live_sessions/week7/ReverseMiddle.java
@@ -9,10 +9,36 @@ public class ReverseMiddle {
 
         String str = words[1];
 
-        for (int i = 0; i < str.length(); i++) {
-
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
         }
 
+        words[1] = reverse;
+        System.out.println(Arrays.toString(words)); // it prints as an array [, , ]
+
+        for (String word : words) {
+            System.out.print(word + " "); // it prints independent , not in array
+        }
+        System.out.println(); // to jump up to the next line !!(after print() we should do it !!)
+
+        System.out.println("-----------");
+
+        String[] words2 = {"more", "java", "please"};
+
+        String str1 = words2[1];
+
+        String reverse1 = "";
+        for (int i = str1.length() - 1; i >= 0; i--) {
+            reverse1 += str1.charAt(i);
+        }
+
+        words2[1] = reverse1;
+
+        System.out.println(Arrays.toString(words2));// print as an array [, , ]
+        for (String s : words2) {
+            System.out.print(s + " "); // it prints independent , not in array
+        }
     }
 }
 
