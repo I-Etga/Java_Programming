94e95d83046fd240c1c4bda6bf177603f2b33c1a
i.etga <akayetga@gmail.com>
2023-03-29 05:53:50 +0200

    daily update

--- src/daily_videos/day22_wrapperClasses/MinMaxNumber.java
+++ src/daily_videos/day22_wrapperClasses/MinMaxNumber.java
@@ -1,4 +1,24 @@
 package daily_videos.day22_wrapperClasses;
 
 public class MinMaxNumber {
+
+    public static void main(String[] args) {
+        int [][] numbers = {{100,20,300},{10,1000,50},{-200,400,0}};
+
+        int min= 2147483647;
+        int max =-2147483648;
+
+        for (int[] numbers1D : numbers) {
+            for (int num : numbers1D) {
+                if(num > max) {
+                    max = num;
+                }
+                if(num <min){
+                    min = num;
+                }
+            }
+        }
+        System.out.println("Minimum is " + min);
+        System.out.println("Maximum is " + max);
+    }
 }
