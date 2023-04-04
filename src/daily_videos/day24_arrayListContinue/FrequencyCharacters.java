9c035d369f97b4faaf599e7ebb32711823f20cf4
i.etga <akayetga@gmail.com>
2023-04-04 15:03:47 +0200

    daily update

---
+++ src/daily_videos/day24_arrayListContinue/FrequencyCharacters.java
@@ -0,0 +1,19 @@
+package daily_videos.day24_arrayListContinue;
+
+import java.util.*;
+
+public class FrequencyCharacters {
+    public static void main(String[] args) {
+        String str = "aaabbbcccdddeeebb";
+
+        String result = "";
+        for (String each : str.split("")) {
+
+            int frequency = Collections.frequency(Arrays.asList(str.split("")), each);// So I get the frequency of each element
+            if (!result.contains(each)) {
+                result += each + "" + frequency;
+            }
+        }
+        System.out.println(result);
+    }
+}
