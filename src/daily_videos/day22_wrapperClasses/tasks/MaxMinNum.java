d07d2dab9adbd9f90d64d9ddd2733a4b06bf37d8
i.etga <akayetga@gmail.com>
2023-04-04 13:52:06 +0200

    daily update

--- src/daily_videos/day22_wrapperClasses/tasks/MaxMinNum.java
+++ src/daily_videos/day22_wrapperClasses/tasks/MaxMinNum.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class MaxMinNum {
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(123, 1, 2, 3, 4, 5, 5, 6, 7, 8, 1, 2,-4, 4, 5));
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 5, 6, 7, 8, 1, 2,-4, 4, 5,123));
 
         int max = Integer.MIN_VALUE; //numbers.get(0); 1 2 3 4 5 6 7 8 123 [in order to get the element at specified index , numbers[0] illegal-X]
         int min = Integer.MAX_VALUE; //numbers.get(0); 1 4
