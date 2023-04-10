b82a7354d56a6e5c870adc1544f06fdedecab6ad
i.etga <akayetga@gmail.com>
2023-04-10 21:25:01 +0200

    daily update

---
+++ src/daily_videos/day27_accessModifiers/InstanceBlock.java
@@ -0,0 +1,14 @@
+package daily_videos.day27_accessModifiers;
+
+public class InstanceBlock {
+    {
+        System.out.println("Instance Block"); // everytime we create an object, instances block gets executed. We can also here initialize instance variables.
+    }
+    public InstanceBlock(){
+        System.out.println("Costructor");
+    }
+
+    public static void main(String[] args) {
+        new InstanceBlock();
+    }
+}
