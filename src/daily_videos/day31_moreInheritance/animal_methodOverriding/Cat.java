49bbf81865c7f04454f080e40672fea82b30f2b1
i.etga <akayetga@gmail.com>
2023-04-19 01:46:17 +0200

    daily update

---
+++ src/daily_videos/day31_moreInheritance/animal_methodOverriding/Cat.java
@@ -0,0 +1,36 @@
+package daily_videos.day31_moreInheritance.animal_methodOverriding;
+
+public class Cat extends Animal{
+
+    public Cat(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override // After that statement , if the compiler does not give error, then it means it, we have override the method successfully !!
+    public void eat(){
+        System.out.println("Cat " + getName() + " is eating cat food");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Cat " + getName() + " sleeps 12 hours in a day" );
+    }
+
+    //@Override --> gives error because it's a unique method !!
+    public void scratch(){
+        System.out.println("Cat " + getName() + " is scratching" );
+    }
+
+    /*
+    If you call the Cat object, then hat eat method will get executed.
+    However, if we call the Animal object, then the eat method of Animal obj will get executed !!
+
+    #1 same TYPE , same PARAMETERS , same NAME
+    #2 static, final , private NOT ALLOWED
+    #3 access modifier of the overridden method needs to be same of more visible !! for example: public -> must be public --> Because it's the most visible access modifier !!
+
+    ** if the parameters are different, name is same --> OVERLOADING !!
+     */
+
+
+}
