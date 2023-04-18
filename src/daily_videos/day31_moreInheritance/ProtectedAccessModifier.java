d226a76f1e4418969a74ee89978b6dea147f7279
i.etga <akayetga@gmail.com>
2023-04-18 19:33:41 +0200

    daily update

---
+++ src/daily_videos/day31_moreInheritance/ProtectedAccessModifier.java
@@ -0,0 +1,9 @@
+package daily_videos.day31_moreInheritance;
+
+public class ProtectedAccessModifier {
+
+    protected static int a = 100;
+    protected static void method1(){
+
+    }
+}
---
+++ src/tests/TestAccessModifier.java
@@ -0,0 +1,17 @@
+package tests;
+
+import daily_videos.day31_moreInheritance.ProtectedAccessModifier;
+
+public class TestAccessModifier  extends ProtectedAccessModifier{
+
+    public static void main(String[] args) {
+
+        // We can't access protected variables and methods in different directory.
+
+        // But if we extend the class where the protected variables and methods come from, we can access !!
+        // Because as we talked about before, protecteds can be reachable in different packet,only if it's subclasses of it !!
+
+        System.out.println(ProtectedAccessModifier.a);
+        ProtectedAccessModifier.method1();
+    }
+}
