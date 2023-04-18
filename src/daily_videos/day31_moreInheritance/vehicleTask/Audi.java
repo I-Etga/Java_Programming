c5ac55ef2a24248e87fb0e2aeb91ed28fe7b080d
i.etga <akayetga@gmail.com>
2023-04-18 19:19:32 +0200

    daily update

---
+++ src/daily_videos/day31_moreInheritance/vehicleTask/Audi.java
@@ -0,0 +1,7 @@
+package daily_videos.day31_moreInheritance.vehicleTask;
+
+public class Audi extends Car{
+    public Audi(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+}
---
+++ src/daily_videos/day31_moreInheritance/vehicleTask/Yacht.java
@@ -0,0 +1,7 @@
+package daily_videos.day31_moreInheritance.vehicleTask;
+
+public class Yacht extends Boat{
+    public Yacht(String brand, String model, String color, int year, double price) {
+        super(brand, model, color, year, price);
+    }
+}
