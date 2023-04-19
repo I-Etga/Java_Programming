973a1d2d07a487f84b9c4c73ba8a8eaf9b5989f7
i.etga <akayetga@gmail.com>
2023-04-19 02:30:09 +0200

    daily update

---
+++ src/daily_videos/day31_moreInheritance/shape_methodOverriding/Circle.java
@@ -0,0 +1,48 @@
+package daily_videos.day31_moreInheritance.shape_methodOverriding;
+
+import utilities.MathUtil;
+
+public class Circle extends Shape {
+
+    public double radius;
+
+    // Constructor of Shape class is called implicitly. Because it has no argument [default] !!
+    public Circle(double radius) {
+        setRadius(radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        // super.area() --> it's 0(default). So I don't want to use it. I just remove it !!
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+        // code fragments that can draw a circle !!
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+                "name='" + getName() + '\'' +
+                "radius='" + getRadius() + '\'' +
+                ",area='" + area() + '\'' +
+                ",perimeter='" + perimeter() + '\'' +
+                '}';
+    }
+}
---
+++ src/daily_videos/day31_moreInheritance/shape_methodOverriding/TestShapeObjects.java
@@ -0,0 +1,18 @@
+package daily_videos.day31_moreInheritance.shape_methodOverriding;
+
+public class TestShapeObjects {
+    public static void main(String[] args) {
+
+        Square square = new Square(5);
+
+        System.out.println(square);
+
+        square.draw();
+
+        System.out.println("--------------");
+
+        Circle circle = new Circle(6);
+        System.out.println(circle);
+
+    }
+}
