defe394980cf172bf5e2956f9c70e07acaf39a99
i.etga <akayetga@gmail.com>
2023-04-22 00:20:58 +0200

    daily update

--- src/sundays_live_sessions/week_11/access/a/Windows.java
+++ src/sundays_live_sessions/week_11/access/a/Windows.java
@@ -6,11 +6,15 @@ public class Windows extends Computer{
 
     public static void main(String[] args) {
 
+        // IN THE SAME PACKAGE BUT IN DIFFERENT CLASS
+
         Windows windows = new Windows();
-        windows.os = "Windows";
-        windows.brand = "Microsoft";
-        windows.memory = 8;
-        // windows.hasWifiCard = true;  It's private. So I can access in different class even if it's subclass !! [However, getter/setter methods help for that !!]
+
+        windows.os = "Windows"; // it's public. --> accessible #1in the same class // #2in the same package but different class // #3 in different package #4 in the World (I don't know what that is now )
+        windows.brand = "Microsoft"; // it's protected. -->  accessible #1in the same class // #2in the same package but different class // #3 !! in different package only if it's SUBCLASS !!
+        windows.memory = 8; // // it's default -->  accessible ONLY #1 in the same class or #2 in the same package but different class
+
+        // windows.hasWifiCard = true;  It's private. I can access only in the same class! So I can't access in different class even if it's subclass !! [However, getter/setter methods help for that !!]
 
     }
 }
--- src/sundays_live_sessions/week_11/access/b/Mac.java
+++ src/sundays_live_sessions/week_11/access/b/Mac.java
@@ -7,10 +7,10 @@ public class Mac extends Computer {
     public static void main(String[] args) {
         Mac mac = new Mac();
 
-        // IN DIFFERENT PACKAGE BUT NOT SUBCLASS !!
+        // IN DIFFERENT PACKAGE AND  SUBCLASS !!
 
         mac.os = "MacOS"; // it's public. --> accessible #1in the same class // #2in the same package but different class // #3 in different package #4 in the World (I don't know what that is now )
-        mac.memory = 8; // it's protected. -->  accessible #1in the same class // #2in the same package but different class // #3 in different package only if it's SUBCLASS
+        mac.memory = 8; // it's protected. -->  accessible #1in the same class // #2in the same package but different class // #3 !! in different package only if it's SUBCLASS !!
         //mac.brand = "Apple"; // it's default -->  accessible ONLY #1 in the same class or #2 in the same package
         //mac.hasWifiCard = true; // it's private. --> accessible ONLY in the same class (exception getter/setter)
     }
