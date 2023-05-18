1307ebfbac06a9f236e0a9abb9e79eae43af698a
i.etga <akayetga@gmail.com>
2023-05-18 05:05:12 +0200

    Day 35 updated

--- src/daily_videos/day35_polymorphism/PolymorphismPractice2.java
+++ src/daily_videos/day35_polymorphism/PolymorphismPractice2.java
@@ -1,5 +1,7 @@
 package daily_videos.day35_polymorphism;
 
+import daily_videos.day29_inheritance.animalTask.*;
+import daily_videos.day30_inheritanceContinue.phoneTask.*;
 import daily_videos.day33_abstraction.employeeTask.*;
 
 public class PolymorphismPractice2 {
@@ -64,6 +66,70 @@ public class PolymorphismPractice2 {
                 System.out.println(each.getName());
             }
         }
+
+
+        Animal animal = new Animal();//not polymorphism
+        // There must be IS A inheritance relationship !!
+        //# reference type        # obj type
+        Animal cat = new Cat();//polymorphism
+
+        Cat cat1 = new Cat(); // not polymorphism
+
+
+        // upcasting [parent->sub] (implicitly)
+        Animal dog = new Dog();
+        Animal tiger = new Tiger();
+
+        animal.setName("Any"); // not polymorphism
+        animal.eat();
+        animal.drink();
+        animal.sleep();
+
+        System.out.println("******");
+
+        cat.setName("Pamuk");//polymorphism--> We can access methods and variables of Animal Class BUT with Cat Class implementation !!
+        // No uniques of Cat class because of polymorphism
+        cat.eat();
+        cat.drink();
+        cat.sleep();
+
+        System.out.println("******");
+
+        cat1.setName("Duman");
+        cat1.eat();
+        cat1.drink();
+        cat1.sleep();
+        cat1.scratch();
+        cat1.meow();
+
+        // #downcasting [sub->parent] (explicitly)
+
+        // Animal dog = new Dog();
+        Dog dog1 = (Dog) new Animal();
+        dog1.bark();
+
+
+        /* primitive casting
+
+        long a = 567842490;
+         int b = (int) a;
+
+         int c = 45;
+         long d = c;
+
+         */
+
+        Phone phone = new Nokia("XR20", "Small", "Blue", 350);
+
+        ((Nokia) phone).selfDefence(); // I can call it through downcasting.
+
+        ((IPhone) phone).faceTime(911); // ClassCastException
+
+        // the obj type of dog object is Dog Class.
+        // Both of them are subclasses of Animal Class.
+        // However, there is no IS A inheritance relationship between dog and cat.
+
+        // ((Cat) dog).eat(); // unchecked -runtime exception
 
 
     }
