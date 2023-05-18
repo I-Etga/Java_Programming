2a09ac3f29ba6cd5e4436250758e60f07905ef81
i.etga <akayetga@gmail.com>
2023-05-18 22:44:24 +0200

    Day 39 List & Set

--- src/daily_videos/day39_collections/ListDemo.java
+++ src/daily_videos/day39_collections/ListDemo.java
@@ -1,15 +1,18 @@
 package daily_videos.day39_collections;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+
 
 public class ListDemo {
 
     public static void main(String[] args) {
 
+        System.out.println("***** Array List *****");
+
         List<Integer> arrayList1 = new ArrayList<>(); // polymorphism
-        arrayList1.get(0);
+        //List has index number, allows duplicates [1,1,4,2,4,1...]
+
+        // arrayList1.get(0);
         // As you remember, reference type decides which variables and methods can be accessible.
         // So .get(index)  obviously comes from parent class named List !!
 
@@ -18,7 +21,6 @@ public class ListDemo {
         // You can take a look at ArrayList and LinkedList Class in order to see that different implementations
 
         ArrayList<Integer> arrayList = new ArrayList<>(); // Array
-        arrayList.get(0);
         // ArrayList uses implicitly array.
         // If we call the get() method, ArrayList's get method is faster than other .
         // Because it's using Array.
@@ -29,10 +31,14 @@ public class ListDemo {
 
         arrayList.remove(0);
 
+        System.out.println(arrayList.get(0));
+
+
+        System.out.println("***** Linked List *****");
         LinkedList<Integer> linkedList = new LinkedList<>(); // Node based class[Doubly linked List][Elements are linked to previous and next elements]
         // adding , inserting , removing is faster ==> add((), addAll(), remove(), removeAll(),removeIf(), retainAll()
 
-        linkedList.get(0);
+
         // get method of LinkedList is not as fast as ArrayList.
         //Retrieving the element in ArrayList is also faster than LinkedList.
 
@@ -42,11 +48,53 @@ public class ListDemo {
 
         linkedList.remove(0);
 
-        System.out.println("***** ***** *****");
+        System.out.println(linkedList.get(0));
+
+        System.out.println("***** Vector *****");
+
+        Vector<Integer> vector = new Vector<>();
+        // Class methods and objects are synchronized [thread safety]!!
+        vector.add(100);
+        vector.add(200);
+        vector.add(300);
+
+        System.out.println(vector.get(0));
+
+        System.out.println("***** Stack *****");
+        // Stack is child class of Vector Class
+        // Class, methods and objects are synchronized [thread safety]!!
+
+        Stack<Integer> stack = new Stack<>();
+        stack.add(100);
+        stack.add(200);
+        stack.add(300);
+
+        System.out.println(stack);
+
+        int lastElement = stack.pop(); // Pop methods returns and then removes the last element of Stack !!
+
+        System.out.println(stack);
+
+        int secondLastElement = stack.pop();
+
+        System.out.println(secondLastElement);
 
 
+        /*1. List (I): Has Index Number, Allows Duplicate
 
+                1.1 ArrayList: Array based class
+                               Accessing/searching the elements are faster
 
+                1.2 LinkedList: Node based class (doubly linked list)
+                                Insertion & deletion of elements are faster
+
+                1.3 Vector: Same as ArrayList but synchronized
+                            Is Synchronized ==> Thread-safety
+
+                1.3.1 Stack: Same as ArrayList but synchronized
+                            Last-In-First-Out (LIFO) ===> pop()
+
+         */
     }
 
 
---
+++ src/daily_videos/day39_collections/SetDemo.java
@@ -0,0 +1,104 @@
+package daily_videos.day39_collections;
+
+import java.util.*;
+
+public class SetDemo {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(); // accepts duplicates, keeps insertion order, has index
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        System.out.println(list); // [10, 200, 300, 40, 90, 10, 200, 300, 40, 90, 10, 200, 300, 40, 90, 10, 200, 300, 40, 90] ==> allows duplicates
+
+        System.out.println("----------------------------------------");
+        // Set is subclass of Collection and that's why it has addAll() method !!
+        Set<Integer> set1 = new HashSet<>(); // order is random, accepts null key
+        set1.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set1.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set1.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set1.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set1.addAll(Arrays.asList(null, null, null, null, null)); // only one "null" is accepted. Because it does NOT allow duplicates.8
+
+        System.out.println(set1); // [null, 200, 40, 10, 90, 300] ==> NO duplicates !!
+
+        System.out.println("----------------------------------------");
+
+        Set<Integer> set2 = new LinkedHashSet<>();  // keeps insertion order, accepts null key
+        set2.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set2.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set2.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set2.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set2.addAll(Arrays.asList(null, null, null, null, null));
+
+        System.out.println(set2);
+
+
+        System.out.println("----------------------------------------");
+
+        String str = null;
+        // System.out.println(str.toLowerCase());[NullPointerExceptions]
+        // Because we can't call method through null keyword !!
+
+
+        Set<Integer> set3 = new TreeSet<>(); // Keeps sorted order, does not accept duplicate
+
+
+        set3.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set3.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set3.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        set3.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        //  set3.addAll(Arrays.asList(null, null, null, null, null));
+
+        // Sorted order. It checks the values of each object.
+        //We can't call method through null keyword !!
+        // That's why in TreeSet we are not allowed to add null keyword !![NullPointerExceptions]
+
+        System.out.println(set3); // [10, 40, 90, 200, 300] (sorted !!)
+
+
+        System.out.println("----------------------------------------");
+        String[] words = {"Java", "Java", "Java", "Python", "Python", "C#", "C++", "Ruby", "C#", "C#"};
+
+        Set<String> result = new LinkedHashSet<>();
+        result.addAll(Arrays.asList(words)); // keeps insertion order. If it is not LinkedHashSet(e.g. HashSet or TreeSet), order wouldn't be kept !!
+
+        System.out.println(result);
+
+        /*
+        // System.out.println(result.get(1));
+         NO index and that's why no retrieve. So no get method !!
+        (If you check the Collection common methods, you'll see no get method in the table !!)
+
+        for (String each : result) {
+            System.out.println(each);
+        }
+        */
+
+        System.out.println(new ArrayList<>(result).get(1));
+
+        words = result.toArray(new String[0]);  // converting Set to Array !!!!
+        //
+
+
+        System.out.println("Words Array : " + Arrays.toString(words));
+
+
+        System.out.println("----------------------------------------");
+
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.addAll(Arrays.asList(10, 10, 10, 20, 20, 30, 30, 30, 40, 40, 40, 50, 50, 50, 5, 5, 5, 1, 1, 1, 2, 2, 2));
+
+        Set<Integer> n = new TreeSet<>(numbers); // List to Set [removes duplicates and sorts (order)]
+
+        System.out.println(n);
+
+
+        // numbers = new ArrayList<>(n);
+
+
+    }
+}
